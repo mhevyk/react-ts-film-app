8afbf805608deb2e36aa88876355a1cd9a6d0c86
mhevyk <maxxevyk7@gmail.com>
2023-12-26 14:10:26 +0200

    feat: Splitted code to client-side and server-side

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       "@images": resolveSource("assets", "images"),
       "@utils": resolveSource("utils"),
       "@theme": resolveSource("theme"),
+      "@api": resolveSource("api"),
     },
   },
   plugins: [react()],
