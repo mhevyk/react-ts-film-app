a5ae4e222f54844b2df65aae79f195befb7fe0d7
mhevyk <maxxevyk7@gmail.com>
2023-12-26 13:42:47 +0200

    feat: Added Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren } from "react";
+import styled from "styled-components";
+import shevronRightIcon from "@icons/chevron-right.svg";
+import { Link } from "react-router-dom";
+
+const TitleWrapperLink = styled(Link)`
+  display: flex;
+  gap: 8px;
+  padding: 24px 0 24px 32px;
+`;
+
+const Title = styled.h5`
+  margin: 0;
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+`;
+
+const ShevronRightIcon = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+
+type SectionProps = PropsWithChildren & {
+  title: string;
+  navigatesTo: string;
+};
+
+export function Section({ title, navigatesTo, children }: SectionProps) {
+  return (
+    <section>
+      <TitleWrapperLink to={navigatesTo}>
+        <Title>{title}</Title>
+        <ShevronRightIcon src={shevronRightIcon} alt="Shevron Right icon" />
+      </TitleWrapperLink>
+      {children}
+    </section>
+  );
+}
