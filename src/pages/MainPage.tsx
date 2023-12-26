620560f61516cf474f460b961654268276ade712
mhevyk <maxxevyk7@gmail.com>
2023-12-26 12:39:00 +0200

    feat: Composed layout for sliders

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,56 @@
+import { Slider } from "@components/Slider";
+import { media } from "@theme/mediaQueries";
+import styled from "styled-components";
+
+const Header = styled.header`
+  position: sticky;
+  z-index: 1;
+  top: 0;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  z-index: 5;
+  padding: 50px 0 0; // TODO: remove top scacing, its just for test
+  background-color: rgb(${({ theme }) => theme.colors.backgroundRGB});
+
+  ${media.screens.md} {
+    padding: 50px ${({ theme }) => theme.globals.contentContainerSpacing} 0; // TODO: remove top scacing, its just for test
+  }
+
+  & > div {
+    // TODO: remove later, its just for test
+    margin-bottom: 50px;
+  }
+`;
+
 export function MainPage() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Header>
+        <Slider
+          variant="full-screen"
+          slides={[1, 2, 3]}
+          renderSlide={(slide) => `Full-screen slide ${slide}`}
+          pagination
+          navigationControls
+          autoplay
+        />
+      </Header>
+      <ContentWrapper>
+        <Slider
+          variant="medium"
+          slides={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          renderSlide={(slide) => `Small slide ${slide}`}
+          navigationControls
+        />
+        <Slider
+          variant="small"
+          slides={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          renderSlide={(slide) => `Mediaum slide ${slide}`}
+          navigationControls
+        />
+      </ContentWrapper>
+    </>
+  );
 }
