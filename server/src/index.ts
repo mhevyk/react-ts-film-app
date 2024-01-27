7b7c1ae5e1590e8b55c22048d0343fdcf9d23221
mhevyk <maxxevyk7@gmail.com>
2024-01-27 15:21:38 +0200

    Init server

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}/`);
+});
