e6ed0d3602b2b181766e20324e12edbf666311af
mhevyk <maxxevyk7@gmail.com>
2024-02-03 23:25:38 +0200

    feat: Added type helper to expand type during development

--- client/src/types/helpers.ts
+++ client/src/types/helpers.ts
@@ -7,3 +7,5 @@ export type StyledPick<T, K extends keyof T> = PrefixWith$<Pick<T, K>>;
 export type AsProps<T extends ElementType> = {
   as?: T;
 } & ComponentPropsWithoutRef<T>;
+
+export type Expand<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
