ced56d9cd36771f03d46de637e50a6f4e20f2156
mhevyk <maxxevyk7@gmail.com>
2024-02-25 11:27:13 +0200

    feat: Added scroll restoration to all pages

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import { Footer } from "./Footer";
 
 export function RootLayout() {
   return (
     <>
+      <ScrollRestoration />
       <Sidebar />
       <Outlet />
       <Footer />
